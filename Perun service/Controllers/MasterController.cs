016fef0cd31bb74e351e6c85afbb832c3e626341
Ivan Stoychev <ivanstoychev44@gmail.com>
2020-06-21 02:17:43 +0300

    Added "MasterController" class and "RestSharp" and "Useful.String.Extensions" nugets.

---
+++ Perun service/Controllers/MasterController.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Perun_service.Controllers
+{
+    /// <summary>
+    /// Orchestrates all API calls for this service.
+    /// </summary>
+    class MasterController
+    {
+    }
+}
